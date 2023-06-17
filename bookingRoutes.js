7e8d400ce80989d4ec95cad0761da4f70592b92d
Amka Tsendjargal <47069150+amka-tsendjargal@users.noreply.github.com>
2023-06-17 12:35:23 -0600

    Comments Added

--- bookingRoutes.js
+++ bookingRoutes.js
@@ -1,6 +1,17 @@
+// This router file handles the booking page.
+// The page displays user's selected package information
+// Takes in user's payment information and adds it to database
+// After payment, it adds the bought package to user's records
+// Author: Amka, Shuo
+// When: June 2023
+
+// Necessary module imports
 const express = require("express");
 const mysql = require("mysql");
 const router = express.Router();
+
+// This function generates a random string of 6 to 9 letters and numbers to be used as the booking number
+// (DOES NOT YET CHECK FOR DUPLICATES)
 function generateRandomString() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   const length = Math.floor(Math.random() * 4) + 6; // Random length between 6 and 9
@@ -12,13 +23,16 @@ function generateRandomString() {
   return randomString;
 }
 
+// Create a connection to the database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "xiangshuo",
   password: "password",
   database: "travelexperts",
 });
-let pkg_id;
+let pkg_id; // Storing which package they selected from the previous page by its id
+
+// This get method displays database information about previously selected package
 router.get("/booking", (req, res) => {
   pkg_id = req.query.id;
   var sql= "SELECT `PackageId`, `PkgName`, `PkgStartDate`, `PkgEndDate`, `PkgDesc`, `PkgBasePrice`, `PkgAgencyCommission`,DATEDIFF(`PkgEndDate`,`PkgStartDate`) as days,MONTHNAME(`PkgStartDate`) AS month,DAY(`PkgStartDate`) AS SDAY, DAY(`PkgEndDate`) AS EDAY FROM packages WHERE `PackageId` = ?";
@@ -29,6 +43,9 @@ router.get("/booking", (req, res) => {
   });
 });
 
+// This post method adds the appropriate info into the 'bookings', 'bookingdetails', and 'creditcards' tables
+// The INSERT into bookings table occurs seperately before INSERT into 'bookingdetails' so we use the 'bookings' last insert id
+// to get details needed for 'bookingdetail' information
 router.post('/submit-payment', (req, res) => {
   const connection = mysql.createConnection({
     host: "localhost",
@@ -45,7 +62,7 @@ router.post('/submit-payment', (req, res) => {
   const bookingQuery = "INSERT INTO `bookings`(`BookingId`, `BookingDate`, `BookingNo`, `CustomerId`) VALUES (?, ?, ?, ?)"
   connection.query({'sql': bookingQuery, 'values': [0, new Date(), generateRandomString(), customerId]}, (err, result) => {
     if (err) throw err;
-    lastBookingId = result.insertId;
+    lastBookingId = result.insertId; // Storing id of last entry into 'bookings' table
     console.log('Booking entry successful')
   });
  
