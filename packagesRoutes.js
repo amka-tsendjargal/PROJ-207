25b7e94edfdc09ed1070e269a8ecf2eebbf432b7
deepathoppil14 <133913830+deepathoppil14@users.noreply.github.com>
2023-06-18 12:09:21 -0600

    comment added

--- packagesRoutes.js
+++ packagesRoutes.js
@@ -19,10 +19,11 @@ router.get('/packages', (request, response) => {
     dbh.connect((err)=>{
     
         console.log('connected');
+        //Fetching all package details from packages table.
         var sql= "SELECT `PackageId`, `PkgName`, `PkgStartDate`, `PkgEndDate`, `PkgDesc`, `PkgBasePrice`, `PkgAgencyCommission`,DATEDIFF(`PkgEndDate`,`PkgStartDate`) as days,MONTHNAME(`PkgStartDate`) AS month,DAY(`PkgStartDate`) AS SDAY, DAY(`PkgEndDate`) AS EDAY FROM packages";
         dbh.query(sql,(err,result,field)=>{
             if(err) throw err;
-            
+            //send the package information to client side
             response.render("packages", {"pageTitle": "Packages","pkg_array":result})
             dbh.end((err)=>{
                 if(err) throw err;
